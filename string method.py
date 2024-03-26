3dc8dbc7df11cd08747ab03bb92aea8ebe331a59
obito48 <mbrakibulislam48372>
2024-03-26 13:57:01 +0600

    String method

---
+++ string method.py
@@ -0,0 +1,15 @@
+a = 'Harry !!!!!! Harry' #string are immutable
+print(len(a))
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.rstrip('!'))
+print(a.replace('Harry','Jhon'))
+print(a.split(' '))
+blogHeading = 'introduction to jS'
+print(blogHeading.capitalize())
+
+str1 = 'welcome to the console!!!'
+print(len(str1))
+print(len(str1.center(50)))
+print(a.count('Harry'))
