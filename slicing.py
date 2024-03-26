c22831d54344e426d3a8b96b685b7c2923d76402
obito48 <mbrakibulislam48372>
2024-03-26 13:48:22 +0600

    String slicing

---
+++ slicing.py
@@ -0,0 +1,9 @@
+fruit = "mango"
+mangolen = len(fruit)
+print(mangolen)
+print(fruit[0:1]) #including 0 but not 4
+print(fruit[1:4])#including 1 but not 4
+print(fruit[:5])
+print(fruit[0:-3])
+print(fruit[-1:-3])
+print(fruit[-3:-1]) 
