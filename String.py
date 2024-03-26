01cf8621abf59420c1685800c5e4b0769efdfbc9
obito48 <mbrakibulislam48372>
2024-03-26 13:23:53 +0600

    Some string

---
+++ String.py
@@ -0,0 +1,18 @@
+a = complex(8,2,)
+a1 = 4
+print(a)
+b = "Harry"
+print (a+a1)
+print("The tyep of a is", type(a))
+print("The tyep of a is", type(b))
+print("The tyep of a is", type(a1))
+
+
+list1 = [8, 2.3, [-4, 5], ["apple","bannana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"),("lion", "tiger"))
+print(tuple1)
+
+dict1 = {"name":"saksi", "age":20,"canVote":True}
+print(dict1)
