d093d901d4461fdf0ac359bac7247da472926887
obito48 <mbrakibulislam48372>
2024-03-26 13:42:40 +0600

    String User In Python

---
+++ Stringuser.py
@@ -0,0 +1,20 @@
+name ="Rakib"
+Friend = "naim"
+anotherfriend = 'Sakib'
+apple = '''He said, 
+Hi harry
+how are you 
+i am good 
+"I want eat an apple'''
+
+print("Hello," + name)
+print(apple)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+#print(name[5])#Throus an error
+print("Lets use a for loop\n")
+for character in name:
+  print(character)
